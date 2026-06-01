933f293fbcde97876b1163a7f5c7c00495ba13b8
riccimatteo874 <riccimatteosepaniagultom@gmail.com>
2026-06-01 15:20:50 +0700

    tes

--- tugas_akhir.py
+++ tugas_akhir.py
@@ -1,31 +1,46 @@
+#====================================================================
+
+# CLASS PLAYER, ENEMY, ITEM, SKILL
 class Entitas:
-  def __init__(self, nama, hp = 100, attack = 10):
+  def __init__(self, nama, hp=100, attack=10):
     self.nama = nama
     self.hp = hp
     self.max_hp = hp
     self.attack = attack
 
+class NodeSkill:
+  def __init__(self, nama_skill):
+    self.nama = nama_skill
+    self.left = None
+    self.right = None
+
 class Player(Entitas):
-  def __init__(self, nama, role = 'fighter'):
+  def __init__(self, nama, role='fighter'):
     super().__init__(nama)
-    self.nama = nama
     self.exp = 0
+    self.score = 0
     self.level = 1
     self.role = role
     self.gold = 0
-    self.inventory = None
+    self.inventory = Inventory()
+    self.skill_tree = NodeSkill('Lvl 1: Tebasan Pedang')
+    self.buruan = set()
 
     if role == 'tank':
-      self.hp =  150
+      self.hp = 150
+      self.max_hp = 150
       self.attack = 5
+      self.skill_tree = NodeSkill('Lvl 1: Hantaman Perisai')
     elif role == 'marksman':
       self.hp = 80
+      self.max_hp = 80
       self.attack = 15
+      self.skill_tree = NodeSkill('Lvl 1: Panah Kilat')
 
 class Enemy(Entitas):
-  def __init__(self, nama, hp, attack, reward):
+  def __init__(self, nama, hp, attack, reward_gold):
     super().__init__(nama, hp, attack)
-    self.reward = reward
+    self.reward_gold = reward_gold
 
 class Item:
   def __init__(self, nama, jenis, power, harga):
@@ -34,39 +49,88 @@ class Item:
     self.power = power
     self.harga = harga
 
+class Inventory:
+  def __init__(self):
+    self.items = []
+
+  def tambah_item(self, item):
+    self.items.append(item)
+
+  def tampilkan_item(self):
+    if not self.items:
+      return False
+    for i, item in enumerate(self.items):
+      print(f'{i+1}. {item.nama}: {item.jenis} + {item.power}')
+    return True
+
+  def hapus_item(self, index):
+    if 0 < index <= len(self.items):
+      return self.items.pop(index-1)
+    return None
+
 toko = [
   Item('heal potion', 'heal', 30, 30),
   Item('attack potion', 'buff', 10, 30)
 ]
 
-class Login(): #hash table
-  def __init__(self, size = 10):
-    self.size = size
-    self.table = [None] * size
+#================================================
 
-  def hash(self, key):
-    jumlah = 0
-    for i in key:
-      jumlah += ord(i)
-    return jumlah % self.size
+# CLASS UNTUK LEADERBOARD ENEMY
+class NodeEnemy: # BINARY SEARCH TREE
+  def __init__(self, enemy):
+    self.enemy = enemy
+    self.left = None
+    self.right = None
 
-  def register(self, username, pw):
-    index = self.hash(username)
-    self.table[index] = pw
+class BountyBoard: # BINARY SEARCH TREE
+  def __init__(self):
+    self.root = None
 
-  def cek(self, username, pw):
-    index = self.hash(username)
-    return self.table[index] == pw
+  def insert(self, enemy):
+    if self.root is None:
+      self.root = NodeEnemy(enemy)
+    else:
+      self.rekursif_insert(self.root, enemy)
+
+  def rekursif_insert(self, node, enemy):
+    if enemy.reward_gold < node.enemy.reward_gold:
+      if node.left is None:
+        node.left = NodeEnemy(enemy)
+      else:
+        self.rekursif_insert(node.left, enemy)
+    elif enemy.reward_gold > node.enemy.reward_gold:
+      if node.right is None:
+        node.right = NodeEnemy(enemy)
+      else:
+        self.rekursif_insert(node.right, enemy)
+
+  def tampilkan_board(self, node, nomor = None):
+    if nomor is None:
+      nomor = [1]
+    if node:
+      self.tampilkan_board(node.right, nomor)
+      print(f"{nomor[0]}. {node.enemy.nama} | HP: {node.enemy.hp} | Atk: {node.enemy.attack} | Hadiah: {node.enemy.reward_gold} Gold") 
+      nomor[0] += 1
+      self.tampilkan_board(node.left, nomor)
   
+  def search_target(self, node, target_gold):
+    if node is None or node.enemy.reward_gold == target_gold:
+      return node
+    if target_gold < node.enemy.reward_gold:
+      return self.search_target(node.left, target_gold)
+    return self.search_target(node.right, target_gold)
+#===================================================================================================================================
+
+# CLASS UNTUK DATA PEMAIN
 class NodePlayer(): #single linked list
   def __init__(self, player):
     self.player = player
     self.next = None
 
-class PlayerList(): # single linked list
+class PlayerList(): # SINGLE LINKED LIST
   def __init__(self):
     self.head = None
-  
+
   def tambah_pemain(self, player):
     new = NodePlayer(player)
     new.next = self.head
@@ -79,74 +143,4 @@ class PlayerList(): # single linked list
         return temp.player
       temp = temp.next
     return None
-
-def main():
-  lala = Login()
-  listpemain = PlayerList()
-  p_aktif = None
-  while True:
-    if not p_aktif:
-      print("\n=== BATTLE ARENA (PROGRES 20%) ===")
-      print("1. Login\n2. Register\n3. Keluar Game")
-      opsi = int(input("Pilih menu: "))
-      if opsi == 1:
-        user = input('username:')
-        pas = input('password: ')
-        if lala.cek(user, pas):
-          p_aktif = listpemain.get_pemain(user)
-          if p_aktif:
-            print(f'\nLogin sukses. Welcome {p_aktif.nama}')
-        else:
-          print('Login gagal')
-
-      elif opsi == 2:
-        user_baru = input('Username:')
-        pas_baru = input('Password: ')
-        while True:
-            try:
-              print('Pilih role:\n' \
-              '1. fighter\n' \
-              '2. tank\n' \
-              '3. marksman\n')
-              t = int(input('Pilih: '))
-              if t == 1:
-                role = 'fighter'
-                break
-              elif t == 2:
-                role = 'tank'
-                break
-              elif t == 3:
-                role = 'marksman'
-                break
-              else:
-                print('Pilih antara 1/2/3!!')
-            except ValueError:
-              print('Masukkan angka 1/2/3')
-        new_player = Player(user_baru, role)
-        lala.register(user_baru, pas_baru)
-        listpemain.tambah_pemain(new_player)
-        print('Registrasi berhasil')
-
-      elif opsi == 3:
-        print("Keluar")
-        break
-    
-    else:
-      print(f"\n=== MAIN MENU | Player: {p_aktif.nama} ({p_aktif.role}) ===")
-      print("1. Battle (Tahap Pengembangan)")
-      print("2. Toko (Tahap Pengembangan)")
-      print("3. Logout")
-
-      opsi = int(input('Pilih: '))
-      if opsi == 1:
-        print('Belum selesai')
-      elif opsi == 2:
-        print('Belum selesai')
-      elif opsi == 3:
-        p_aktif = None
-        print('Logout')
-
-if __name__ == "__main__":
-    main()
-
-
+#================================================================================================
